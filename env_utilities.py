2a30183e4a34531bacd98ab26eb6931c3c24ece0
Jack S. <jlogan2210@gmail.com>
2025-12-08 13:06:51 -0600

    fix section name/add test code for environment

--- env_utilities.py
+++ env_utilities.py
@@ -8,21 +8,21 @@ def create_environment(render=False):
         env = gym.make("CartPole-v1", render_mode="human")
     else:
         env = gym.make("CartPole-v1")
-        return env
+    return env
 
 
 def random_episode(env, max_steps=500):
     # Run a random episode in the environment and return reward.
     obs, info = env.reset()
-    total_reward = 0.0
+    final_reward = 0.0
     steps = 0
 
     for t in range(max_steps):
         action = env.action_space.sample()  # Sample random action
-        obs, reward, terminated, trunicated, info = env.step(action)
+        obs, reward, terminated, truncated, info = env.step(action)
         final_reward += reward
         steps += 1
-        done = terminated or trunicated
+        done = terminated or truncated
         if done:
             break
     return final_reward, steps
---
+++ training_scripts/environment_test.py
@@ -0,0 +1,28 @@
+import time
+from env_utilities import create_environment, random_episode
+
+"""
+Optional test sctipt to verify environment functionality. Run using:
+py -m training_scripts.environment_test
+In repo root.
+"""
+
+
+def main():
+    render = True
+
+    env = create_environment(render=render)
+
+    num_episodes = 5
+    for episode in range(1, num_episodes + 1):
+        final_reward, steps = random_episode(env)
+        print(
+            f"Episode {episode}/// Total Reward: {final_reward:.2f} in {steps} steps.")
+
+        if render:
+            time.sleep(0.75)  # Pause between episodes
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
