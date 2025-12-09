379ce546afa6969630ac4a9b53d918a5f0520d70
Jack S. <jlogan2210@gmail.com>
2025-12-08 18:13:41 -0600

    add baseline model

---
+++ models/MLP_baseline.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class MLPBase(nn.module):
+    def __init__(self, state_in=4, hidden_lay=64, action_out=2):
+        super().__init()
+
+        self.network = nn.Sequential(
+            nn.Linear(state_in, hidden_lay),
+            nn.ReLU(),
+            nn.Linear(hidden_lay, hidden_lay),
+            nn.ReLU(),
+            nn.Linear(hidden_lay, action_out),
+        )
+
+    def forward(self, state: torch.Tensor):
+        if state.dim() == 1:
+            state = state.unsqueeze(0)
+        return self.network(state)
+
+    def action(self, state: torch.Tensor, deterministic=True):
+        logits = self.forward(state)
+        if deterministic:
+            return torch.argmax(logits, dim=-1).item()
+        else:
+            probabilities = torch.softmax(logits, dim=-1)
+            distribution = torch.distributions.Categorical(probabilities)
+            action = distribution.sample().item()
+        return action
